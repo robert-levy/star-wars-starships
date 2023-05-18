f24330175cd3af9afaddea14ec7b036cac462cfa
Robert Levy <36302718+robert-levy@users.noreply.github.com>
2023-05-19 00:01:42 +0100

    added readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Star Wars Starships
+## Simple app that retrieves data on starships and renders them within cards
+![Image](./src//assets/app-screenshot.PNG)
--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import styled from "styled-components";
 import Header from './components/Header/Header'
 import SpaceshipList from "./components/SpaceshipList/SpaceshipList";
 import Footer from "./components/Footer/Footer";
+import spaceBackground from './assets/space-bg.jpg'
 const StylingWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content:space-between;
   align-items:center;
-  background-image: url('./src/assets/space-bg.jpg');
+  background-image: url(${spaceBackground});
   width:100%;
   height: 100%;
   text-align: center;
